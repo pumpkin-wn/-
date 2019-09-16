cd459a865a7033ffb388adb7da43fbdf7fe95bc6
pumpkin-wn <2729735630@qq.com>
2019-09-16 21:19:52 +0800

    danli.java

---
+++ danli.java
@@ -0,0 +1,8 @@
+class Single{
+    //自己创建一个对象
+      private static Single INSTANCE=new Single();
+      private SIngle(){}                                            //私有化构造方法
+      public static Single getInstance(){                 //提供返回该对象的静态方法
+                return INSTANCE;
+      }
+}
